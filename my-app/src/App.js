c3c42457bad58e8089cebcce5e637d2e29211812
annemarieselin <156382573+annemarieselin@users.noreply.github.com>
2024-03-24 09:06:50 -0700

    Added styling elements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,29 @@ function App() {
       <header className="App-header">
         <h3>Enter city...</h3>
         <SearchInput />
+        <footer class="sources">
+          This site was coded by Anne-Marie Selin at{" "}
+          <a href="https://selinmarketing.com/" target="_blank">
+            Selin Marketing
+          </a>{" "}
+          and is open-source on{" "}
+          <a
+            href="https://github.com/annemarieselin/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://react-weather-app-annemarieselin.netlify.app/"
+            target="_blank"
+          >
+            {" "}
+            Netlify
+          </a>
+          .
+        </footer>
       </header>
     </div>
   );
