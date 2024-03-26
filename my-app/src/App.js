a2117ccf7f963e96909d8304602339802e7bd0f9
annemarieselin <156382573+annemarieselin@users.noreply.github.com>
2024-03-26 16:04:54 -0700

    Changed sources

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,15 +8,20 @@ function App() {
       <header className="App-header">
         <h3>🔎 Enter city...</h3>
         <SearchInput />
-        <footer class="sources">
+        <footer className="sources">
           This site was coded by Anne-Marie Selin at{" "}
-          <a href="https://selinmarketing.com/" target="_blank">
+          <a
+            href="https://selinmarketing.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Selin Marketing
           </a>{" "}
           and is open-source on{" "}
           <a
             href="https://github.com/annemarieselin/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             {" "}
             Github
@@ -25,6 +30,7 @@ function App() {
           <a
             href="https://react-weather-app-annemarieselin.netlify.app/"
             target="_blank"
+            rel="noreferrer"
           >
             {" "}
             Netlify
