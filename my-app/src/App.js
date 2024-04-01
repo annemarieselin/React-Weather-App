99d0831c347c26625f6f296649f0990f7109674d
annemarieselin <156382573+annemarieselin@users.noreply.github.com>
2024-04-01 07:51:42 -0700

    Added default city London

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SearchInput />
+        <SearchInput defaultCity="London" />
       </header>
     </div>
   );
